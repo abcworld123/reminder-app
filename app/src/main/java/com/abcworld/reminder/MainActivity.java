69253b1312723845b987d1f79262e7da1df3ff00
abcworld123 <rkaxhdals@gmail.com>
2022-07-28 10:01:11 +0900

    Add priority set

--- app/src/main/java/com/abcworld/reminder/MainActivity.java
+++ app/src/main/java/com/abcworld/reminder/MainActivity.java
@@ -2,9 +2,7 @@ package com.abcworld.reminder;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +10,9 @@ import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
+
+
+enum Priority { HIGH, MEDIUM }
 
 
 public class MainActivity extends AppCompatActivity
@@ -23,44 +24,66 @@ public class MainActivity extends AppCompatActivity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		String title = "리마인더";
-
 		EditText etTitle = findViewById(R.id.editTextTitle);
 		EditText etContent = findViewById(R.id.editTextContent);
-		EditText etGroup = findViewById(R.id.editTextGroup);
 		RadioGroup radioGroup = findViewById(R.id.radioGroup);
 		Button btn = findViewById(R.id.buttonSave);
 
 		radioGroup.check(R.id.radioMedium);
 
 		btn.setOnClickListener(v -> {
-//			String title = etTitle.getText().toString();
+			String title = etTitle.getText().toString();
 			String body = etContent.getText().toString();
-//			String big = etGroup.getText().toString();
-			sendNotification(title, body);
+			int checked = radioGroup.getCheckedRadioButtonId();
+
+			if (title.equals("")) {
+				title = "리마인더";
+			}
+
+			if (checked == R.id.radioHigh) {
+				notify(title, body, Priority.HIGH);
+			} else {
+				notify(title, body, Priority.MEDIUM);
+			}
 		});
 	}
 
 
-	private void sendNotification(String messageTitle, String messageBody) {
-		Intent intent = new Intent(this, MainActivity.class);
-		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent, PendingIntent.FLAG_IMMUTABLE);
-		String channelId = getString(R.string.default_notification_channel_id);
-		NotificationCompat.Builder notificationBuilder =
-				new NotificationCompat.Builder(this, channelId)
-						.setSmallIcon(R.drawable.bell)
-						.setContentTitle(messageTitle)
-						.setContentText(messageBody)
-						.setAutoCancel(false)
-						.setContentIntent(pendingIntent);
+	private void notify(String title, String content, Priority priority) {
+		final String channelId = getString(R.string.notification_channel_id);
+		final String channelName = "알림";
+		int groupId, icon;
+		String group;
+
+		if (priority == Priority.HIGH) {
+			group = "com.abcworld.reminder.high";
+			groupId = 1;
+			icon = R.drawable.siren;
+		} else {
+			group = "com.abcworld.reminder.medium";
+			groupId = 2;
+			icon = R.drawable.ic_stat_bell;
+		}
+
+		NotificationCompat.Builder groupBuilder = new NotificationCompat.Builder(this, channelId)
+				.setSmallIcon(icon)
+				.setAutoCancel(true)
+				.setOnlyAlertOnce(true)
+				.setGroup(group)
+				.setGroupSummary(true);
+
+		NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId)
+				.setSmallIcon(icon)
+				.setContentTitle(title)
+				.setContentText(content)
+				.setGroup(group)
+				.setAutoCancel(true);
 
 		NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-		String channelName = "일반 알림";
 		NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
 		notificationManager.createNotificationChannel(channel);
 
 		notificationManager.notify(NotificationID.getID(), notificationBuilder.build());
+		notificationManager.notify(groupId, groupBuilder.build());
 	}
 }
